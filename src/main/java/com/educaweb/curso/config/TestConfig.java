2213f326e459004a0705c5f2b523ed448022818a
Madson Santos <madsonsamsung@gmail.com>
2020-05-01 15:04:59 -0300

    Criando operação de instânciação

---
+++ src/main/java/com/educaweb/curso/config/TestConfig.java
@@ -0,0 +1,32 @@
+package com.educaweb.curso.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.educaweb.curso.domain.Categoria;
+import com.educaweb.curso.repositories.CategoriaRepository;
+
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {		
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+
+	
+	
+}
