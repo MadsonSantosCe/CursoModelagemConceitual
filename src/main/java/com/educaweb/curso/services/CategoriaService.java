417e5df583a1536dd90bfe2f160fb6065efecde1
Madson Santos <madsonsamsung@gmail.com>
2020-05-14 13:58:08 -0300

    Atualizando Categoria com PUT

--- src/main/java/com/educaweb/curso/services/CategoriaService.java
+++ src/main/java/com/educaweb/curso/services/CategoriaService.java
@@ -25,4 +25,9 @@ public class CategoriaService {
 		obj.setId(null);
 		return reposity.save(obj);
 	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return reposity.save(obj);
+	}
 }
