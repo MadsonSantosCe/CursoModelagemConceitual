14c3078e4e39d4336e93a6489c9292a151387179
Madson Santos <madsonsamsung@gmail.com>
2020-08-06 10:09:34 -0300

    Inserindo Pedido

--- src/main/java/com/educaweb/curso/services/PedidoService.java
+++ src/main/java/com/educaweb/curso/services/PedidoService.java
@@ -1,7 +1,13 @@
 package com.educaweb.curso.services;
 
+import java.util.Date;
 import java.util.Optional;
 
+import com.educaweb.curso.domain.ItemPedido;
+import com.educaweb.curso.domain.PagamentoComBoleto;
+import com.educaweb.curso.domain.enums.EstadoPagamento;
+import com.educaweb.curso.repositories.ItemPedidoRepository;
+import com.educaweb.curso.repositories.PagamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,10 +20,42 @@ public class PedidoService {
 
 	@Autowired
 	private PedidoRepository repository;
+
+	@Autowired
+	private BoletoService boletoService;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+
+	@Autowired
+	private ProdutoService produtoService;
 	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repository.findById(id);
 		return obj.orElseThrow(()-> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: "+ id + ", Tipo: "+ Pedido.class.getName()));
+	}
+
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repository.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
