a88206226823aca6a4db5e8d543fdf1e3e35a689
Madson Santos <madsonsamsung@gmail.com>
2020-08-13 08:49:26 -0300

    Criando o profile de teste

--- src/main/java/com/educaweb/curso/CursoApplication.java
+++ src/main/java/com/educaweb/curso/CursoApplication.java
@@ -1,12 +1,19 @@
 package com.educaweb.curso;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursoApplication {
+public class CursoApplication  implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+
 }
